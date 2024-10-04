e0d4f8a32e2cb28c3dd7662a49b0850714ff1a60
Mitesh Chaudhari <mitchoudhary15@gmail.com>
2024-10-04 14:58:17 +0530

    fix

--- main.js
+++ main.js
@@ -201,6 +201,7 @@ const loadModel = (modelObj, i) => {
     (xhr) => {
       totalAssetsWeight = xhr;
       loading[i] = xhr.total > 0 ? xhr.loaded / xhr.total : 1;
+      var per = Math.round(Number(loading) * 100) + "%";
 
       if (i == 0) {
         // loadThrottle(loading[i]);
